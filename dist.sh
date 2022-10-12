0c5f17a4d9bef70c82dd985db098d811780c3dc5
sckim0430 <another0430@naver.com>
2022-10-12 12:06:25 +0000

    update file

--- dist.sh
+++ dist.sh
@@ -4,7 +4,7 @@ NCCL_DEBUG=INFO
 NNODES=2
 NODE_RANK=0
 NPROC_PER_NODE=1
-MASTER_ADDR='192.168.0.76'
-MASTER_PORT=12345
+MASTER_ADDR='IP_ADDR_OF_MASTER_NODE'
+MASTER_PORT='FREE_PORT'
 
 python -m torch.distributed.launch --nnodes=${NNODES} --node_rank=${NODE_RANK} --nproc_per_node=${NPROC_PER_NODE} --master_addr=${MASTER_ADDR} --master_port=${MASTER_PORT} distributed_torch.py --gpu-id=0
